7276fb91bc74c3e9041108fca8c71fc907d67351
henriqueeyer <henriqueeyer@outlook.com>
2020-02-13 22:41:09 -0300

    Updates

---
+++ precache-manifest.ef13efe4f01e09e5f956a764a1cc6863.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d48e4f590270722d4ce3a408cbb1df7c",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "459b72471f7b5a6db7fd",
+    "url": "/my-app/static/js/2.25272837.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/my-app/static/js/2.25272837.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0c461ab2dbffbc613b03",
+    "url": "/my-app/static/js/main.5e95b36c.chunk.js"
+  },
+  {
+    "revision": "134c6d7b033e4b816dc9",
+    "url": "/my-app/static/js/runtime-main.56c001b7.js"
+  }
+]);
